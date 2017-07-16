1d2a75e9a1a318b3a98efcb96deb4130b0013a0f
Rodemfa913 <Rodemfa913@rodemfa-PC>
2017-07-16 13:04:41 -0300

    Building main interface -> Adding event for 'Edit' button ->-> Rectangle property dialog created.

---
+++ DrawingFX/src/dwgfx/view/shape/RectProps.java
@@ -0,0 +1,41 @@
+package dwgfx.view.shape;
+
+import dwgfx.view.Props;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Spinner;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * Controller class for Rectangle subcomponent of Properties dialog.
+ *
+ * @author Rodemfa913
+ */
+public class RectProps implements Props {
+    @FXML private Spinner<Double> arcHeightSpin;
+    @FXML private Spinner<Double> arcWidthSpin;
+    @FXML private Spinner<Double> heightSpin;
+    @FXML private Spinner<Double> widthSpin;
+    @FXML private Spinner<Double> xSpin;
+    @FXML private Spinner<Double> ySpin;
+    private Rectangle rect;
+    
+    @Override public void setItem(Node item) {
+        rect = (Rectangle) item;
+        xSpin.getValueFactory().setValue(rect.getLayoutX());
+        ySpin.getValueFactory().setValue(rect.getLayoutY());
+        widthSpin.getValueFactory().setValue(rect.getWidth());
+        heightSpin.getValueFactory().setValue(rect.getHeight());
+        arcWidthSpin.getValueFactory().setValue(rect.getArcWidth());
+        arcHeightSpin.getValueFactory().setValue(rect.getArcHeight());
+    }
+
+    @Override public void handleApply() {
+        rect.setLayoutX(xSpin.getValue());
+        rect.setLayoutY(ySpin.getValue());
+        rect.setWidth(widthSpin.getValue());
+        rect.setHeight(heightSpin.getValue());
+        rect.setArcWidth(arcWidthSpin.getValue());
+        rect.setArcHeight(arcHeightSpin.getValue());
+    }
+}
--- DrawingFX/src/dwgfx/view/shape/ShapeProps.java
+++ DrawingFX/src/dwgfx/view/shape/ShapeProps.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.*;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 
@@ -15,11 +16,14 @@ import javafx.scene.shape.*;
  * @author rodemfa
  */
 public class ShapeProps implements Initializable, Props {
+    @FXML private Accordion shapeRoot;
     @FXML private ColorPicker fillColorPicker;
     @FXML private ColorPicker strokeColorPicker;
     @FXML private ComboBox<StrokeLineCap> lineCapCombo;
     @FXML private ComboBox<StrokeLineJoin> lineJoinCombo;
     @FXML private Spinner<Double> strokeWidthSpin;
+    @FXML private TitledPane genTitledPane;
+    private Props controller;
     private Shape shape;
     
     /**
@@ -29,19 +33,27 @@ public class ShapeProps implements Initializable, Props {
      * @param rb
      */
     @Override public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        shapeRoot.setExpandedPane(genTitledPane);
     }
 
-    @Override public void setItem(Node item) {
+    @Override public void setItem(Node item) throws Exception {
         shape = (Shape) item;
         fillColorPicker.setValue((Color) shape.getFill());
         strokeColorPicker.setValue((Color) shape.getStroke());
         strokeWidthSpin.getValueFactory().setValue(shape.getStrokeWidth());
         lineCapCombo.setValue(shape.getStrokeLineCap());
         lineJoinCombo.setValue(shape.getStrokeLineJoin());
+        FXMLLoader loader = new FXMLLoader();
+        String filePath = "RectProps.fxml";
+        loader.setLocation(getClass().getResource(filePath));
+        Pane root = loader.load();
+        genTitledPane.setContent(root);
+        controller = loader.getController();
+        controller.setItem(shape);
     }
 
     @Override public void handleApply() throws Exception {
+        controller.handleApply();
         shape.setFill(fillColorPicker.getValue());
         shape.setStroke(strokeColorPicker.getValue());
         shape.setStrokeWidth(strokeWidthSpin.getValue());
